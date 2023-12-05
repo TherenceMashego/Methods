f682ef644b4800da593e76c82e5caa3adca6d0e8
TERRENCE  MASHEGO <TERRENCE  MASHEGO@USERDELLFLOOR2>
2023-12-05 16:22:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calling a method, this can be done many times; methods and functions
+            // are literally the same in C#, preferably call them methods
+            SayHi();
+
+            // Method with parameters 
+            SayHiUser("Evans");
+
+            // Method with more parameters
+            SayHiAge("John", 55);
+
+            // FREEZING THE CONSOLE
+            Console.ReadLine();
+        }
+
+        static void SayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        static void SayHiUser(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        static void SayHiAge(string name, int age)
+        {
+            Console.WriteLine($"Hello {name}, You are {age} years old.");
+        }
+    }
+}
